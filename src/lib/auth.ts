21ce0d1f610119256d43f6b4bced67c6f4993c23
parlamas <parlamas@live.com>
2025-12-31 10:46:50 +0100

    20251231-1046

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,4 +1,4 @@
-//src/lib/auth.ts
+// src/lib/auth.ts
 
 import "server-only";
 import type { NextAuthOptions } from "next-auth";
@@ -9,7 +9,23 @@ import { prisma } from "@/lib/prisma.server";
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
+
   session: { strategy: "jwt" },
+
+  cookies: {
+    sessionToken: {
+      name:
+        process.env.NODE_ENV === "production"
+          ? "__Secure-next-auth.session-token"
+          : "next-auth.session-token",
+      options: {
+        httpOnly: true,
+        sameSite: "lax",
+        path: "/",
+        secure: process.env.NODE_ENV === "production",
+      },
+    },
+  },
 
   providers: [
     CredentialsProvider({
